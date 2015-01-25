a9ee9c650e41e85dae890620524b91a7a3c1ab80
knx <knx@digitalheads.pl>
2015-01-25 14:41:24 +0100

    Tweaks
    * added new_app function
    * added pg_start function
    * tweaked the bash theme
    * added brew install script

--- .bash/plugins/ruby.plugin.bash
+++ .bash/plugins/ruby.plugin.bash
@@ -12,3 +12,10 @@ cat_colorless() {
   cat $1 | sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g"
 }
 
+new_app() {
+  rails new $1 -m https://raw.github.com/RailsApps/rails-composer/master/composer.rb
+}
+
+pg_start() {
+  pg_ctl start -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log
+}
