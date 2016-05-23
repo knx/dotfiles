08432557b33b20445220ac3d192893bffe791f93
Krzysztof Niksiński <knx@digitalheads.pl>
2016-05-23 18:16:34 +0200

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,25 @@
+cd
+
+# clone
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+git clone ssh://github.com/knx/dotfiles.git ~/.dotfiles
+
+# install
+~/.bash_it/install.sh --interactive
+
+# links
+mv .bashrc .bashrc.bak
+mv .bash_profile .bash_profile.bak
+ln -s ~/.dotfiles/.bash_profile
+ln -s ~/.dotfiles/.bashrc
+ln -s ~/.dotfiles/.vimrc
+
+# vim and theme and aliases
+mkdir .vim
+ln -s ~/.dotfiles/colors ~/.vim/
+ln -s ~/.dotfiles/mine ~/.bash_it/themes/
+ln -s ~/.dotfiles/custom.bash ~/.bash_it/custom/
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
